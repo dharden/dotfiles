1635abda09057416edb1bd7b004b1ca2d2f73713
Harden <hardend@a45e60e1e619.ant.amazon.com>
2015-07-30 10:59:08 -0700

    Dropping some stuff in a homebrew script, mostly for notes later.

---
+++ scripts/homebrew.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+brew install tmux
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install slack
+brew cask install spotify
+if commandexists xcodebuild; then sudo xcodebuild -license
+brew install macvim --override-system-vim && brew linkapps
