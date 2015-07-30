b0a9aee68950cbd4d15ff47efec4fe88a8aa62f4
Daniel Harden <dharden@zappos.com>
2015-07-30 11:25:58 -0700

    couple other things while I'm thinking about it

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -3,9 +3,11 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install git
 brew install tmux
+brew install mosh
 brew install caskroom/cask/brew-cask
 brew cask install google-chrome
 brew cask install slack
 brew cask install spotify
+brew cask install nosleep
 if commandexists xcodebuild; then sudo xcodebuild -license
 brew install macvim --override-system-vim && brew linkapps
